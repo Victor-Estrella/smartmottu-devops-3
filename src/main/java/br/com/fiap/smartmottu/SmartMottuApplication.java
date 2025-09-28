a64582cc7228d4349d75f7cae0525ef518bafa6e
Victor-Estrella <victorhcarracci@gmail.com>
2025-09-28 00:16:02 -0300

    feat: add SmartMottuApplication and SmartMottuApplicationTests for application setup and testing

--- src/main/java/br/com/fiap/smartmottu/SmartMottuApplication.java
+++ src/main/java/br/com/fiap/smartmottu/SmartMottuApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafehubApplication {
+public class SmartMottuApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafehubApplication.class, args);
+		SpringApplication.run(SmartMottuApplication .class, args);
 	}
 }
--- src/test/java/br/com/fiap/smartmottu/SmartMottuApplicationTests.java
+++ src/test/java/br/com/fiap/smartmottu/SmartMottuApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class SmarMottuApplicationTests {
+class SmartMottuApplicationTests {
 
 	@Test
 	void contextLoads() {
