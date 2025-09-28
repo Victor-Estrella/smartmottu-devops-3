95d6ca38ffa7a271798e8bf6a5ec53c391c0cf7c
Victor-Estrella <victorhcarracci@gmail.com>
2025-09-28 00:28:26 -0300

    feat: exclude SecurityAutoConfiguration in tests and clean up application-test.properties

--- src/test/java/br/com/fiap/smartmottu/SmartMottuApplicationTests.java
+++ src/test/java/br/com/fiap/smartmottu/SmartMottuApplicationTests.java
@@ -1,10 +1,11 @@
 package br.com.fiap.smartmottu;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(exclude = SecurityAutoConfiguration.class)
 @ActiveProfiles("test")
 class SmartMottuApplicationTests {
 
